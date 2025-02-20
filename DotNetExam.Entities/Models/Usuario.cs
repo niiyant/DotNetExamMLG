3a24b5abe65d994994223bac3707badc516e6d30
NiiyaNT <angelmendoza59@gmail.com>
2025-02-20 15:03:59 -0600

    Añadir propiedad PasswordSalt a Usuario
    Se ha añadido una nueva propiedad `PasswordSalt` de tipo `byte[]` a la clase `Usuario` en el espacio de nombres `DotNetExam.Entities.Models`.

--- DotNetExam.Entities/Models/Usuario.cs
+++ DotNetExam.Entities/Models/Usuario.cs
@@ -12,5 +12,6 @@ namespace DotNetExam.Entities.Models
         public string Nombre { get; set; }
         public string Email { get; set; }
         public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
     }
 }
