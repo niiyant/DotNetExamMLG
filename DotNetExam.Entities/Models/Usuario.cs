0b57c7d96d2b8fa71f5e8e7024160e87f8f49ed3
NiiyaNT <angelmendoza59@gmail.com>
2025-02-20 14:17:38 -0600

    Cambio de tipo de dato en PasswordHash de Usuario
    Se ha modificado la propiedad `PasswordHash` en la clase `Usuario`, cambiando su tipo de `string` a `byte[]`. Esto permite almacenar el hash de la contraseña en formato binario, mejorando la seguridad al evitar el almacenamiento en texto plano.

--- DotNetExam.Entities/Models/Usuario.cs
+++ DotNetExam.Entities/Models/Usuario.cs
@@ -11,6 +11,6 @@ namespace DotNetExam.Entities.Models
         public int Id { get; set; }
         public string Nombre { get; set; }
         public string Email { get; set; }
-        public string PasswordHash { get; set; }
+        public byte[] PasswordHash { get; set; }
     }
 }
