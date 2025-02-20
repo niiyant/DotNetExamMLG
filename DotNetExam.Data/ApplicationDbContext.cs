607d5c65d053a01e1b52ee1cbced542a1cb0a900
NiiyaNT <angelmendoza59@gmail.com>
2025-02-20 15:49:19 -0600

    Añadir datos iniciales y mejorar estilos de artículos
    Se han añadido datos iniciales para las entidades `Tienda`, `Articulo` y `TiendaArticulo` en `ApplicationDbContext.cs`.
    
    Se han realizado cambios en el estilo de los elementos de los artículos en `dashboard.component.css`, incluyendo una nueva clase para las imágenes.
    
    Además, se ha actualizado la plantilla en `dashboard.component.html` para mostrar las imágenes de los artículos con la nueva clase CSS `articulo-imagen`.

--- DotNetExam.Data/ApplicationDbContext.cs
+++ DotNetExam.Data/ApplicationDbContext.cs
@@ -53,6 +53,23 @@ namespace DotNetExam.Data
             modelBuilder.Entity<Articulo>()
                 .Property(a => a.Precio)
                 .HasPrecision(18, 2);
+            var tienda1 = new Tienda { Id = 1, Sucursal = "Tienda A", Direccion = "Calle 123" };
+            var tienda2 = new Tienda { Id = 2, Sucursal = "Tienda B", Direccion = "Avenida 456" };
+            var articulo1 = new Articulo { Id = 1, Codigo = "ART001",Imagen = "https://http2.mlstatic.com/D_NQ_NP_869818-MLU77724314601_072024-O.webp", Descripcion = "Laptop", Precio = 1500.00m, Stock = 10 };
+            var articulo2 = new Articulo { Id = 2, Codigo = "ART002", Imagen = "https://http2.mlstatic.com/D_NQ_NP_877416-CBT77139740446_062024-O.webp", Descripcion = "Smartphone", Precio = 800.00m, Stock = 20 };
+            var articulo3 = new Articulo { Id = 3, Codigo = "ART003", Imagen = "https://http2.mlstatic.com/D_NQ_NP_709058-MLU78169795366_082024-O.webp", Descripcion = "Tablet", Precio = 400.00m, Stock = 15 };
+            var articulo4 = new Articulo { Id = 4, Codigo = "ART004", Imagen = "https://http2.mlstatic.com/D_NQ_NP_886158-MLA46737528644_072021-O.webp", Descripcion = "Monitor", Precio = 250.00m, Stock = 25 };
+
+            var tiendaArticulos = new List<TiendaArticulo>
+            {
+                new TiendaArticulo { TiendaId = 1, ArticuloId = 1, Fecha = DateTime.Now },
+                new TiendaArticulo { TiendaId = 1, ArticuloId = 2, Fecha = DateTime.Now },
+                new TiendaArticulo { TiendaId = 2, ArticuloId = 3, Fecha = DateTime.Now },
+                new TiendaArticulo { TiendaId = 2, ArticuloId = 4, Fecha = DateTime.Now }
+            };
+            modelBuilder.Entity<Tienda>().HasData(tienda1, tienda2);
+            modelBuilder.Entity<Articulo>().HasData(articulo1, articulo2, articulo3, articulo4);
+            modelBuilder.Entity<TiendaArticulo>().HasData(tiendaArticulos);
         }
     }
 }
